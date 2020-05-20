e5364af2d2730a2e35c57ca598f85429995ce69f
root <root@CT100.gateway>
2020-05-20 04:17:00 +0000

    Bot trends upwards or downwards to look more realistic

--- trading-bot.py
+++ trading-bot.py
@@ -10,15 +10,27 @@ import config
 
 client = ExchangeClient(config.apiid, config.secret)
 
-with open ('parameters.json', 'r') as parameterFile:
-    data=parameterFile.read()
-
-parameters = json.loads(data)
-minTrade = int(parameters['minTrade'])
-maxTrade = int(parameters['maxTrade'])
-minWaitTimeMS = int(parameters['minWaitTimeMS'])
-maxWaitTimeMS = int(parameters['maxWaitTimeMS'])
+minTrade = -1
+maxTrade = -1
+minWaitTimeMS = -1
+maxWaitTimeMS = -1
 pair = "SKYM/USDT"
+
+
+def updateParameters():
+    global minTrade
+    global maxTrade
+    global minWaitTimeMS
+    global maxWaitTimeMS
+
+    with open ('parameters.json', 'r') as parameterFile:
+        data=parameterFile.read()
+
+    parameters = json.loads(data)
+    minTrade = int(parameters['minTrade'])
+    maxTrade = int(parameters['maxTrade'])
+    minWaitTimeMS = int(parameters['minWaitTimeMS'])
+    maxWaitTimeMS = int(parameters['maxWaitTimeMS'])
 
 tradeNumber = 0
 def sellAndBuyOrders(bids, asks, exchangeAmount):
@@ -32,22 +44,28 @@ def sellAndBuyOrders(bids, asks, exchangeAmount):
     print("Ask price: "  + str(askPrice))
 
     askPrice = min(float(lastPrice) * 1.49, askPrice)
-    range = 10
-    price = round(bidPrice + float(random.randint(1, range-1)) * (askPrice - bidPrice) / float(range), 6)
 
+    range = 10
+    upwardsScale = 1.01
+    downwardsScale = 0.99
+    if float(lastPrice) * upwardsScale < askPrice and float(lastPrice) * upwardsScale > bidPrice:
+        print("Using adjusted price (upward)")
+        price = float(lastPrice) * upwardsScale
+    elif float(lastPrice) * downwardsScale > bidPrice and float(lastPrice) * downwardsScale < askPrice:
+        print("Using adjusted price (downward)")
+        price = float(lastPrice) * downwardsScale
+    else:
+        price = round(bidPrice + float(random.randint(1, range-1)) * (askPrice - bidPrice) / float(range), 6)
+
+    price = round(price, 6)
     buy = 1
     sell = 2
     limitOrder = 1
     print("ORDER: ")
     print(price)
-    if tradeNumber % 2 is 0:
-        # Sell first then buy
-        print( client.order_place(pair, sell, price, exchangeAmount, limitOrder, None, None) )
-        print( client.order_place(pair, buy, price, exchangeAmount, limitOrder, None, None) )
-    else:
-        # Buy first then sell
-        print( client.order_place(pair, buy, price, exchangeAmount, limitOrder, None, None) )
-        print( client.order_place(pair, sell, price, exchangeAmount, limitOrder, None, None) )
+    # Sell first then buy
+    print( client.order_place(pair, sell, price, exchangeAmount, limitOrder, None, None) )
+    print( client.order_place(pair, buy, price, exchangeAmount, limitOrder, None, None) )
 
     tradeNumber = tradeNumber + 1
 
@@ -64,8 +82,19 @@ def successfulTrade(orderbook):
 
 def runTrades():
     while(True):
+        updateParameters()
         orderBook = client.order_book(pair, 5)['data']
         successfulTrade(orderBook)
         time.sleep(random.randint(minWaitTimeMS, maxWaitTimeMS) / 1000)
 
 runTrades()
+def closeBotsOrders():
+    botsOpenOrders = client.open_orders(None, None)['data']
+    orderIds = []
+    for i in range(len(botsOpenOrders)):
+        orderIds.append(botsOpenOrders[i]['orderId'])
+        if len(orderIds) == 10:
+            print(client.batch_cancel(orderIds))
+            orderIds = []
+
+#closeBotsOrders()
